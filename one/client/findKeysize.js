efe10da2dfe40a53adb3eecae51c5765a527cd09
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-17 16:23:08 -0700

    keysize suspected at 20

---
+++ one/client/findKeysize.js
@@ -0,0 +1,7 @@
+var findKey = require('../break/findKeysize');
+var hamming = require('../break/hamming');
+var fs = require('fs');
+
+fs.readFile('../data/q6.txt',function(err,data){
+  console.log(findKey(data,hamming,{start:3}));
+});
