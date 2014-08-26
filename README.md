46e20ad0bdeac4fee18ba133b0295e21f0448fae
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-25 22:11:44 -0700

    start of q8 and README updated

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-(Matasano Crypto Challenges)[http://cryptopals.com/] done in Javascript. It's not
-so bad outside the browser, honest.
+[Matasano Crypto Challenges](http://cryptopals.com/) done in Javascript.
+
+It's not so bad outside the browser, honest.
---
+++ one/break/hiddenAes.js
@@ -0,0 +1,14 @@
+var fs =require('fs');
+var crypto = require('crypto');
+
+fs.readFile('../data/q8.txt',function(err,data){
+  var str = data.toString();
+  var buf = new Buffer(str,'hex');
+  
+  var decipher = crypto.createDecipheriv('aes-128-ecb',new Buffer('YELLOW SUBMARINE'), new Buffer(0));
+
+  for(var i=0;i<data.length;i+=16){
+    console.log(decipher.update(data,'','utf8'));
+  }
+  decipher.final()
+})
