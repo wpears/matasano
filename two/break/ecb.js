034b12aef6fefdee1d196b18bff35f1e8de001c7
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-30 12:28:40 -0700

    Solved q10 with decCbc. Hackish stuff in break/ecb in order to accept 16 byte chunks, a no-padding flag would have been nice, but as far as I can tell, node doesn't have one natively

--- two/break/ecb.js
+++ two/break/ecb.js
@@ -5,7 +5,9 @@ module.exports = function(block,key){
   var decipher = crypto.createDecipheriv('aes-128-ecb',
       new Buffer(key),
       new Buffer(0));
-      
+  if(block.length === 16){
+      block = Buffer.concat([block,new Buffer('60fa36707e45f499dba0f25b922301a5','hex')],32);
+  }     
   var data = decipher.update(block);
   var fin = decipher.final();
 
