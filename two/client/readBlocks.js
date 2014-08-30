eddebfd78bea24f1a2cffc321e46c39f73bddf63
Wyatt Pearsall <wyattttttt@gmail.com>
2014-08-30 08:44:24 -0700

    readblocks first pass

--- two/client/readBlocks.js
+++ two/client/readBlocks.js
@@ -1,22 +1,36 @@
 var Transform = require('stream').Transform;
 
-function readBlocks(){
+function readBlocks(block){
   Transform.call(this);
+  this.block = block||16;
   this.extra = new Buffer(0);
 }
 
+readBlocks.prototype = Object.create(Transform.prototype);
 
 readBlocks.prototype._transform = function(chk, enc, cb){
+  var block = this.block;
   var extraLen = this.extra.length;
-  var bufStart = 16 - extraLen;
+  var bufStart = block - extraLen;
+  var bufEnd = chk.length - bufStart;
+
+  bufEnd = bufEnd- bufEnd%block;
   
-  this.push(Buffer.concat([this.extra,chk.slice(0,bufStart)],16));
+  this.push(Buffer.concat([this.extra,chk.slice(0,bufStart)],block));
   
-  //move chunk
-   for(var i=bufStart; i < chk.length; i+=16){
-     this.push(chk.slice(i,i+16));
+   var i = bufStart;
+   while(i < bufEnd){
+     this.push(chk.slice(i,i+=block));
    }
-
-
-
+   
+   this.extra = chk.slice(i);
+   
+   cb(); 
 }
+
+readBlocks.prototype._flush = function(cb){
+  if(this.extra.length) this.push(extra);
+  cb();
+}
+
+module.exports = readBlocks;
