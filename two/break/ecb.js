a94448baa73e9f7044e362a9a42f43c54f58eeaa
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-03 19:54:58 -0700

    ecb updated

--- two/break/ecb.js
+++ two/break/ecb.js
@@ -2,14 +2,17 @@ var crypto = require('crypto');
 
 module.exports = function(block,key){
   if(!key) key = 'YELLOW SUBMARINE';
+  if(!(key instanceof Buffer)) key = new Buffer(key);
+
   var decipher = crypto.createDecipheriv('aes-128-ecb',
-      new Buffer(key),
+      key,
       new Buffer(0));
   if(block.length === 16){
       block = Buffer.concat([block,new Buffer('60fa36707e45f499dba0f25b922301a5','hex')],32);
   }     
   var data = decipher.update(block);
   var fin = decipher.final();
+  var len = data.length+fin.length;
 
-  return Buffer.concat([data,fin],data.length+fin.length); 
+  return Buffer.concat([data,fin], len); 
 };
--- two/enc/ecb.js
+++ two/enc/ecb.js
@@ -2,14 +2,17 @@ var crypto = require('crypto');
 
 module.exports = function(block,key){
   if(!(block instanceof Buffer)) block = new Buffer(block)
-  if(!key) key = 'YELLOW SUBMARINE';
+  if(!key) key = new Buffer('YELLOW SUBMARINE');
+  if(!(key instanceof Buffer)) key = new Buffer(key)
 
   var cipher = crypto.createCipheriv('aes-128-ecb',
-      new Buffer(key),
+      key,
       new Buffer(0));
    
   var data = cipher.update(block);
   var fin = cipher.final();
+  var len = data.length+fin.length;
 
-  return Buffer.concat([data,fin],data.length+fin.length);
+  return Buffer.concat([data,fin],len).slice(0,16);
+  
 };
