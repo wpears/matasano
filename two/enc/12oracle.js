8b279cfdc9764dbbd9b107b2abd5ebaae59afc5b
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-07 09:32:17 -0700

    ecb oracle

---
+++ two/enc/12oracle.js
@@ -0,0 +1,16 @@
+var ecb = require('./ecb');
+var crypto = require('crypto');
+var key = crypto.randomBytes(16);
+
+module.exports = function(inp){
+  inp = append(inp);
+   
+  return ecb(inp,key);
+ };
+
+function append(plaintext){
+  if(!(plaintext instanceof Buffer)) plaintext = new Buffer(plaintext);
+  var endStr = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
+  var end = new Buffer(endStr,'base64')
+  return Buffer.concat([plaintext, end],plaintext.length+end.length)
+} 
