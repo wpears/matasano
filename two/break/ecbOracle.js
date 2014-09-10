c9480d7223403af89f68bcea76ea0ba8aa05b1b4
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-09 22:32:39 -0700

    q12 solved, once the first block is discovered, you can use that information to propagate discovery through every block (with the standard block length strategy)

--- two/break/ecbOracle.js
+++ two/break/ecbOracle.js
@@ -37,25 +37,52 @@ function getSecret(size){
   var len = size - 1;
   var known = '';
   while(len >= 0){
-    known+=getByte(len,known);
+    known+=getFirstBytes(len,known);
     len--;
   }
-  console.log(known);
+  var totalLen = oracle(new Buffer(0)).length;
+  while (known.length < totalLen){
+    len = size -1;
+    while(len >= 0){
+      known+=getBytes(len,known);
+      len--;
+    }
+  }
+  return known;
 }
 
-function getByte(len,known){
+function getFirstBytes(len,known){
   var str = new Array(len +  1).join('!');
   var buf = new Buffer(str+known+"X");
+  var obj = makeByteObj(buf);  
+  
+  return findByte(obj,str,known);
+}
+
+function getBytes(len,known){
+  var str = new Array(len +  1).join('!');
+  var buf = new Buffer(known.slice(-15)+"X");
+  var obj = makeByteObj(buf); 
+   
+  return findByte(obj,str,known); 
+}
+
+function makeByteObj(buf){
   var obj = {};
   for(var i = 0; i < 256; i++){
     buf[buf.length-1] = i;
     obj[oracle(buf).slice(0,16).toString()] = i;
   }
+  return obj;
+}
 
-  var thisByte = obj[oracle(new Buffer(str)).slice(0,16).toString()];
+function findByte(obj,str,known){
+  var low = ((known.length/16) >> 0) * 16;
+  var thisByte = obj[oracle(new Buffer(str)).slice(low,low+16).toString()];
   var byteBuf = new Buffer(1);
   byteBuf[0] = thisByte;
   return byteBuf.toString();
 }
-
-
+//Rollin' in my 5.
+//feed 15 -> 0 building object from known.slice(-15).. get the right block
+//
