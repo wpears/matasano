18cb1d45b7a16dcd4a366cc451faa5fbad76ae6e
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-15 22:07:11 -0700

    Padding works in all cases

--- two/client/isValid.js
+++ two/client/isValid.js
@@ -1,12 +1,26 @@
-module.exports = function (block){
+module.exports = function (block, blockLength){
+  if(!blockLength) blockLength = 16;
+
+  if(!block||block.length%blockLength !== 0) throw new Error("Invalid block.");
+  if(!(block instanceof Buffer)) block = new Buffer(block);
+
   var len = block.length -1;
   var last = block[len];
-  var count =1;
+  var count = 1;
 
-  if (last === 0 || last > 16) return 0;
+  if (last === 0 || last > 16) paddingException();
+  if (last === 1) return unpad(block,len);
 
   while (block[--len] === last){
-    if(++count >= last) return 1;
+    if(++count >= last) return unpad(block,len); 
   }
+  paddingException();
+}
+
+function paddingException(){
   throw new Error("Invalid padding. Please don't attack me with this clear padding oracle.")
+}
+
+function unpad(block,len){
+  return block.slice(0,len).toString();
 }
