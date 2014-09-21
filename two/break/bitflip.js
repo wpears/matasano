47b427de7df792b4c277b59cb7af25ed5e403a39
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-21 10:34:02 -0700

    bitflip fleshed out

--- two/break/bitflip.js
+++ two/break/bitflip.js
@@ -6,8 +6,10 @@ var pre = "comment1=cooking%20MCs;userdata=";
 var post = ";comment2=%20like%20a%20pound%20of%20bacon";
 var crypto = require('crypto');
 
-var pk = require('./pkcs7');
-var cbc = require('./cbc');
+var pk = require('../enc/pkcs7');
+var cbc = require('../enc/cbc');
+
+var decCbc = require('./cbc');
 
 var key = crypto.randomBytes(16);
 var IV = crypto.randomBytes(16);
@@ -24,4 +26,11 @@ function encrypt (input){
   var padded = pk(fullString);  
 
   return cbc(padded,key,IV);
+}
+
+function decrypt (enc){
+  var dec = decCbc(enc, key, IV).toString();
+  console.log(dec)
+  return /;admin=true;/.test(dec);
+
 }
