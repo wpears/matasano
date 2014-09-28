3af865e9720129c805c0f5d9f5ee0001821076d9
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-27 17:24:34 -0700

    Provide cbc

---
+++ three/server/padOr.js
@@ -0,0 +1,29 @@
+var strs = ["MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+"MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+"MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+"MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+"MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+"MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+"MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+"MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+"MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+"MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"]
+
+var crypto = require('crypto');
+var enc = require('enc_cbc');
+var dec = require('dec_cbc');
+var pkcs7 = require('pkcs7');
+
+var key = crypto.randomBytes(16);
+var IV = crypto.randomBytes(16);
+
+module.exports = {
+
+  cipher : function(){
+    var str = strs[Math.random()*strs.length>>0];
+    return enc(pkcs7(str),key,IV);
+  },
+  check: function(){
+  }
+
+}
