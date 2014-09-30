f8860290e2693b0e967e9c86088498def2313295
Wyatt Pearsall <wyattttttt@gmail.com>
2014-09-30 14:17:40 -0700

    Almost there, need to suss out some bugs

--- three/break/padOr.js
+++ three/break/padOr.js
@@ -4,47 +4,52 @@ var cipher = serv.cipher();
 var IV = serv.IV;
 var plaintext = new Array (cipher.length);
 cipher = Buffer.concat([IV,cipher]);
-var start = 0;
-var currIndex = 15;
-var currBlock = cipher.slice(start,start+32);
-var interBlock = new Array(16); 
-var padding = 1;
 
-while (currIndex >= 0){
-  console.log(currIndex);
-  var orig = currBlock[currIndex];
-  console.log(currBlock[currIndex+1]);
-  for(var i=0;i<256;i++){
-    currBlock[currIndex] = i;
-    if(serv.check(currBlock)){
-      // the padding is valid;
-      // c1mod == i, p2mod == padding
-      // c1mod ^ p2mod = i2 <-- padding oracle leaks p2mod, thus i2
-      // c1 ^ i2 = p2
-      console.log("Valid");
-      var interState = i ^ padding;
-      interBlock[currIndex] = interState;
-      plaintext[currIndex] = interState^orig;
-      pad(currBlock,interBlock,++padding);
-      console.log("padded");
-      currIndex--;
-      var log = new Buffer(16);
-      plaintext.forEach(function(v,i){
-        log[i]=v;
-      });
-      console.log(log.toString());
-      break;
+
+for(var i=0,len=cipher.length-32; i<= len; i+=16){
+  var currBlock = cipher.slice(i,i+32);
+  processBlock(currBlock,i);
+}
+
+var buf = new Buffer(plaintext.length);
+plaintext.forEach(function(v,i){
+  buf[i]=v;
+});
+console.log(buf.toString());
+
+function processBlock(block,loc){
+  var interBlock = new Array(16);
+  var index = 15;
+  var padding = 1;
+
+  while (index >= 0){
+    console.log(index);
+    var orig = block[index];
+    for(var i=0;i<256;i++){
+      block[index] = i;
+      if(serv.check(block)){
+        // the padding is valid;
+        // c1mod == i, p2mod == padding
+        // c1mod ^ p2mod = i2 <-- padding oracle leaks p2mod, thus i2
+        // c1 ^ i2 = p2
+        var interState = i ^ padding;
+        interBlock[index] = interState;
+        plaintext[loc+index] = interState^orig;
+        pad(block,interBlock,++padding);
+        index--;
+        break;
+      }
+      if(i===255){
+        console.log("uhoh");
+        return;
+      }
     }
-    if(i===255)console.log("uhoh");
   }
 }
 
 function pad(block,interBlock,padding){
-  console.log(arguments);
- 
   var curr = interBlock.length -1;
- while(interBlock[curr] !== undefined){
-   console.log(interBlock[curr]^padding);
+  while(interBlock[curr] !== undefined){
    block[curr] = interBlock[curr] ^ padding;
    curr--;
  }  
