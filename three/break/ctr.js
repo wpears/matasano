785120f911b172197084309f815211c60a3e57c3
Wyatt Pearsall <wyattttttt@gmail.com>
2014-10-01 01:17:42 -0700

    Client for functional ctr

--- three/break/ctr.js
+++ three/break/ctr.js
@@ -1,14 +1,5 @@
 var ecb = require('blockecb');
 var xor = require('xor');
-var inp= new Buffer("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==","base64");
-
-var enc = inp.slice(0,16);
-var inter = ecb("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00","YELLOW SUBMARINE");
-console.log(xor(inter,enc).toString());
-var e2 = inp.slice(16,32);
-var i2 = ecb("\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00","YELLOW SUBMARINE");
-console.log(xor(i2,e2).toString())
-
 
 module.exports = function(buf){
  if(!(buf instanceof Buffer)) buf = new Buffer(buf);
