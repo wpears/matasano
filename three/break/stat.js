f6ec0564065eb2cd8d497d22cdd003fc770c2cca
Wyatt Pearsall <wyattttttt@gmail.com>
2014-10-08 00:18:08 -0700

    decided to do the mundane and break the non-statistical part of q20. on the way

--- three/break/stat.js
+++ three/break/stat.js
@@ -8,10 +8,25 @@ module.exports = function(file,cb){
 function getKey(obj){
   var keyFront = obj.keyFront;
   var ciphers = obj.ciphers;
-  cb(keyFront);
- /* ciphers.forEach(function(v,i){
+  console.log(keyFront.length);
+  keyFront[5]^=79^78;
+  keyFront[22]^=78^82;
+  keyFront[47]^=104^105;
+  keyFront[48] = ciphers[15][48]^69;
+  keyFront[49] = ciphers[15][49]^65;
+  
+  var a = new Buffer(2); 
+  a[0] = ciphers[5][53]^69; 
+  a[1] = ciphers[13][54]^78;
+  //a[2] = ciphers[30][55]^78;
+  //a[3] = ciphers[30][56]^67;
+   
+  keyFront = Buffer.concat([keyFront,a]);
+  ciphers.forEach(function(v,i){
     console.log(i,space(xor(v,keyFront)).toString());
-  });*/
+  });
+  
+  cb(keyFront);
 }
 }
 
